fc7b1a538ffc54c296eed5911fb736c7d933a17d
KoltLBrock <35542746+KoltLBrock@users.noreply.github.com>
2018-01-28 15:41:55 -0600

    Initial Commit

---
+++ app/src/main/java/com/cidm4385/kolton/geoquiz/QuizActivity.java
@@ -0,0 +1,38 @@
+package com.cidm4385.kolton.geoquiz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class QuizActivity extends AppCompatActivity {
+
+    private Button mTrueButton;
+    private Button mFalseButton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz);
+
+        mTrueButton = (Button) findViewById(R.id.true_button);
+        mTrueButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Toast atoast = Toast.makeText(QuizActivity.this, R.string.correct_toast,Toast.LENGTH_SHORT);
+                atoast.setGravity(Gravity.TOP,0,0);
+                atoast.show();
+            }
+        });
+        mFalseButton = (Button) findViewById(R.id.false_button);
+        mFalseButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Toast btoast = Toast.makeText(QuizActivity.this,R.string.incorrect_toast,Toast.LENGTH_SHORT);
+                btoast.setGravity(Gravity.TOP,0,0);
+                btoast.show();
+            }
+        });
+    }
+}
